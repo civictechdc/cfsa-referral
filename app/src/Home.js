60bd5e7542ccf88b04ce8b6399a86bda9f48490e
Michael Kalish <kalishmichael@gmail.com>
2017-09-23 11:19:35 -0400

    Adding bootstrap and a router

---
+++ app/src/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    Container,
+    Row,
+    Col
+} from 'reactstrap';
+
+const Home = () => {
+    return (
+        <Container>
+            <Row>   
+                <Col>
+                    Home Page
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Home;
