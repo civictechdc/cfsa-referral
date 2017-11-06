056a1d2d4c969b7eeef4af29e903ba5ff56c9a83
Michael Kalish <kalishmichael@gmail.com>
2017-11-05 19:54:16 -0500

    Working travis (#40)
    * Better travis
    
    * Fixing broken test

--- app/src/__tests__/Login.js
+++ app/src/__tests__/Login.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import { Login } from '../Login';
 
+const props = {
+    location: {}
+}
+
 describe("Linkbar Component", () => {
   let wrapper
   beforeEach(()=>{
-      wrapper = shallow(<Login />)
+      wrapper = shallow(<Login {...props} />)
   })
   it('renders without crashing', () => {
       expect(wrapper.length).toEqual(1)
