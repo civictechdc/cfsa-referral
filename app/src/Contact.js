995a0b6f1c528cde008f34ff3acda1a2e3d05e3e
Aimee Barciauskas <aimee@developmentseed.org>
2018-02-20 19:51:25 -0500

    Add contact links

--- app/src/Contact.js
+++ app/src/Contact.js
@@ -11,7 +11,7 @@ const Contact = () => {
             <Row className="mt-3">
                 <Col sm={{size: 8, offset: 2}}>
                     <p>
-                        Direct all questions to team@codefordc.org or visit us on Slack!
+                        Direct all questions to team@codefordc.org and dc@datakind.org or visit us on <a href="https://codefordc.org/resources/slack.html">Code for DC</a> and <a href="https://join.slack.com/t/dkdc/shared_invite/enQtMzE4MzI2NzExMjIxLTYzMGEyMmVkMTM2OWFjYzYxNThiM2M4ZWUzZTQ5MmVlMDNmNzY3NDg5NjYyMGI0NzMxN2E0ZjhjN2I4M2M4MWM">DataKind DC</a> Slack!
                     </p>
                 </Col>
             </Row>
